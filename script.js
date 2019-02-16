29f6087d99bc3762436963c7862b9aae11c77331
Qin <1756217895@qq.com>
2019-02-16 19:47:08 +0800

    旋转木马自动旋转并监听事件

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 $(document).ready(() => {
     // 配置使用bootstrap的组件
-   $('.carousel').carousel({
-       interval: 3000
+  
+
+   $('.carousel').on('slide.bs.carousel',(event) => {
+       console.log('slide: ',`
+       方向：${ event.direction }
+       从：${ event.from }
+       到：${ event.to }
+       `)
    })
+
+   //监听slide
+   $('.carousel').on('slid.bs.carousel',(event) => {
+        console.log('slid: ',`
+        方向：${ event.direction }
+        从：${ event.from }
+        到：${ event.to }
+        `)
+    })
+
+    //让旋转木马动起来，放在此处有效果
+    $('.carousel').carousel({
+        interval: 3000
+    })
 
 })
