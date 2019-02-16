dee38c64f279e6fb50ed8c8af00e48260cc467d0
Qin <1756217895@qq.com>
2019-02-16 23:04:01 +0800

    nav 导航

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 $(document).ready(() => {
     // 配置使用bootstrap的组件
-    $('#modal-demo').on('show.bs.modal', function(){
-        console.log('show')
-    })
-    $('#modal-demo').on('shown.bs.modal', function(){
-        console.log('shown')
-    })
-    $('#modal-demo').on('hide.bs.modal', function(){
-        console.log('hide')
-    })
-    $('#modal-demo').on('hidden.bs.modal', function(){
-        console.log('hidden')
-    })
+    
 })
