b78179e674e6c065fa82d488295136f7ab96cead
Qin <1756217895@qq.com>
2019-02-16 21:14:00 +0800

    dropdown方式与事件

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 $(document).ready(() => {
     // 配置使用bootstrap的组件
-  
-
-   $('.carousel').on('slide.bs.carousel',(event) => {
-       console.log('slide: ',`
-       方向：${ event.direction }
-       从：${ event.from }
-       到：${ event.to }
-       `)
-   })
-
-   //监听slide
-   $('.carousel').on('slid.bs.carousel',(event) => {
-        console.log('slid: ',`
-        方向：${ event.direction }
-        从：${ event.from }
-        到：${ event.to }
-        `)
+    $('#dropdown-demo').on('show.bs.dropdown', () => {
+        console.log('show')
     })
-
-    //让旋转木马动起来，放在此处有效果
-    $('.carousel').carousel({
-        interval: 3000
+    $('#dropdown-demo').on('shown.bs.dropdown', () => {
+        console.log('shown')
+    })
+    $('#dropdown-demo').on('hide.bs.dropdown', () => {
+        console.log('hide')
+    })
+    $('#dropdown-demo').on('hidden.bs.dropdown', () => {
+        console.log('hidden')
     })
 
 })
