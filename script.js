6c6f7a014d3bb496bf9f75670df641f08e4b1f69
Qin <1756217895@qq.com>
2019-02-19 22:01:52 +0800

    表单验证

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(() => {
     // 配置使用bootstrap的组件
+    $('#form-demo').submit(function (event) {
+        const form = $(this)
+        if(form[0].checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
+            console.log("表单验证未通过！")
+        }
+
+        form.addClass('was-validated')
+    })
 })
